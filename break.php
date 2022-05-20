6feb412294b87ba872cad61a0a1da616c3a0e4d0
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-20 14:41:44 -0600

    break

--- break.php
+++ break.php
@@ -7,16 +7,28 @@
     <title>break</title>
   </head>
   <body>
-<?php?>
+
 
 <?php
     for($count=0; $count <= 10; $count++){
-    if ($count % 2 == 0 ){continue;}//skip over 5 and loop earlier//skip over even numbers and skip over 0
+    if ($count == 5 ){
+        break;//immediately exits the loop when conditions are met
+    }
     echo $count . ", ";
     }
+    //example of when break is good to use if you are searching 20,000 records and find it in the 100th loop being able to break out of the loop once it is found will save lots of time 
     ?>
-
-
+    <br/>
+    <?php
+for($i=0; $i<=5;$i++){
+            if($i % 2 == 0){continue(1);}//left side of result on screen skipping even numbers and outputting odds on continue it go back through the loop if it is equal to a modulo of %2
+            for($k=0; $k<=5;$k++){//this loopp is outputing the data that is showing on the right side when echoing the data and counts from 0 to 5 and starts over
+                if($k == 3){break(2);}// breaks out of the loop == 3
+                echo $i . "-" .$k ."<br/>";//echo results to the browser
+            }
+        }
+        //*the parenthesis next to break in this instance allow you to break out of booth loops ending them
+?>
 
   </body>
 </html>
