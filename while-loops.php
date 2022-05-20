d6e4e31e8755acc52575896450bcebd349d37563
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-20 11:10:50 -0600

    while loops keeps running while the function is TRUE

--- while-loops.php
+++ while-loops.php
@@ -6,5 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Module 6 loops</title>
   </head>
-  <body></body>
+  <body>
+    <?php
+      $count = 0;
+      while($count <= 10){
+        if ($count == 5){
+          echo "FIVE, ";
+        } else{
+        echo $count . ", ";//if you put the $count in here it will loop infinitly after reaching 5 because it has reached it perameter and has no way to end the function PHP will time out
+      }
+        $count++; //increment by 1
+      } ?>
+      <br/>
+      <?php
+      $scount=0;
+      while($scount <= 15){
+        if ($scount % 2){
+          echo "EVEN, ";
+        } else{
+        echo  "Odd ";//if you put the $count in here it will loop infinitly after reaching 5 because it has reached it perameter and has no way to end the function PHP will time out
+      }
+      if ($scount == 15){
+        echo "STOP";
+      }
+        $scount++; //increment by 1
+      }
+    echo "<br/>";
+    echo "count: {$count}";
+   ?>
+
+
+
+
+
+  </body>
 </html>
