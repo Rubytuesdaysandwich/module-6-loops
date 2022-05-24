62a9c9db896f2c086d03630c1be6d85cd2e5dac8
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-23 19:49:23 -0600

    update

---
+++ Loops/loops_assignment.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Loops assignment</title>
+</head>
+<body>
+    while loop:
+        <br/>
+<?php
+//while loop while count is less than nine output abc
+$abc=0;
+while($abc <= 8 ){
+echo "abc ";
+$abc ++;//increment by 1
+}
+?>
+<br/>
+do-while loop:
+<?php
+//do-while loop
+<do-while>
+
+</do-while>
+
+
+?>
+
+
+
+    
+</body>
+</html>
