3b51162eb44bedfd79fbd34d65ba2cd3edb1d488
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-25 16:19:31 -0600

    fixing spacing

--- Loops/loops_assignment.php
+++ Loops/loops_assignment.php
@@ -64,29 +64,28 @@ PART 3:<br/>
 6 two for make multiplication table:
         <br/>
         <?php
-         echo "<table border =\"2\" style='border-collapse: collapse'>";//gives a table border
+        echo "<table border =\"2\" style='border-collapse: collapse'>";//gives a table border
         for($row=1;$row<=7;$row++){//1 through 7
-       echo "<tr> \n";//table row element defines a row of cells in a table
+        echo "<tr> \n";//table row element defines a row of cells in a table
 
         for($col=1;$col<=7;$col++){//1 through 7 
          $a= $col * $row;// grabing the $col and $row each time they loop and multiplying them and passing the result to the table data cell element
-         echo "<td>$a</td> \n";//puts numbers into columns and boxes a echo the answer from multiplying $col and $row table data cell element
+        echo "<td>$a</td> \n";//puts numbers into columns and boxes a echo the answer from multiplying $col and $row table data cell element
         }
         echo"</tr>";//end of table row elements
 
         }
         echo "</table>";//end of table
-  
         ?>
 PART 4:<br/>
     <?php
     $months = array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");//assigning the months of the years to months
     $daysinmonth = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);//this array is listing how many days are in each month
-        echo "<table border =\"2\" style='border-collapse: collapse'>";// this is giving the border
-         echo "<th colspan=12>Days in a month</th>";//this is heading 
-         $that="";//declaring variable to be added to on line 91
-         $that2="";//declaring variable to be added to on line 92
-         foreach($months as $month =>$currentmonth){//iterating over the items in the loop of $months renamed as $month and $values are accessed using $currentmonth
+    echo "<table border =\"2\" style='border-collapse: collapse'>";// this is giving the border
+        echo "<th colspan=12>Days in a month</th>";//this is heading 
+        $that="";//declaring variable to be added to on line 91
+        $that2="";//declaring variable to be added to on line 92
+        foreach($months as $month =>$currentmonth){//iterating over the items in the loop of $months renamed as $month and $values are accessed using $currentmonth
             
             $that .= "<td>$currentmonth</td>"."\n"; //.=concatenating the nex array item//$currentmonth is pulling the month from the array months
             $that2 .= "<td>$daysinmonth[$month]</td>";//.=concatenating the nex array item//getting the number of days in each month from $daysinmonth
